7468b0ff96df1a4d3550da8fd08d5d5f76b72ecc
JoaoVitorHernandes <149633314+JoaoVitorHernandes@users.noreply.github.com>
2024-11-11 19:25:16 -0300

    Add files via upload
    Atualização Classe PlanoTreino.

--- RA3Design/model/PlanoTreino.java
+++ RA3Design/model/PlanoTreino.java
@@ -1,4 +1,75 @@
 package RA3Design.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class PlanoTreino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String descricao;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataInicio;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataFim;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    @ManyToOne
+    private PersonalTrainer personalTrainer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public PersonalTrainer getPersonalTrainer() {
+        return personalTrainer;
+    }
+
+    public void setPersonalTrainer(PersonalTrainer personalTrainer) {
+        this.personalTrainer = personalTrainer;
+    }
 }
