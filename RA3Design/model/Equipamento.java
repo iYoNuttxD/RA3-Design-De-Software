9c449b47169c56a82170c81437403286fd627292
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-11-12 00:25:53 -0300

    Atualização no Modelo Equipamento
    Arrumado os erros que tinham no modelo Equipamento.

--- RA3Design/model/Equipamento.java
+++ RA3Design/model/Equipamento.java
@@ -6,32 +6,33 @@ import java.util.List;
 
 @Entity
 public class Equipamento {
-    private String nome;
-    private String categoria;
-    private Boolean disponibilidade;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-   
+    private Long id;
+
+    private String nome;
+    private String categoria;
+    private boolean disponibilidade;
 
     @OneToMany(mappedBy = "equipamento")
     private List<Reserva> reservas;
 
-    public Equipamento(String nome, String descricao, int quantidade, List<Reserva> reservas) {
+    public Equipamento(Long id, String nome, String categoria, boolean disponibilidade, List<Reserva> reservas) {
+        this.id = id;
         this.nome = nome;
-        this.descricao = descricao;
-        this.quantidade = quantidade;
+        this.categoria = categoria;
+        this.disponibilidade = disponibilidade;
         this.reservas = reservas;
     }
 
-    public Equipamento() {}
+    public Equipamento(){}
 
-    public int getId() {
+    // Getters e Setters
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -43,20 +44,20 @@ public class Equipamento {
         this.nome = nome;
     }
 
-    public String getDescricao() {
-        return descricao;
+    public String getCategoria() {
+        return categoria;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
     }
 
-    public int getQuantidade() {
-        return quantidade;
+    public boolean isDisponibilidade() {
+        return disponibilidade;
     }
 
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
     }
 
     public List<Reserva> getReservas() {
@@ -65,11 +66,5 @@ public class Equipamento {
 
     public void setReservas(List<Reserva> reservas) {
         this.reservas = reservas;
-    }
-
-    public void setCategoria(String s) {
-    }
-
-    public void setDisponibilidade(boolean b) {
     }
 }
